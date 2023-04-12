c643276a4d279986a289ec867bd8311c46eb6fee
drajatisme <55853547+drajatisme@users.noreply.github.com>
2023-04-12 15:30:37 +0700

    first commit

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"example.com/grpc-tutorial/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	proto.UnimplementedAddServiceServer
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+}
+
+func (s *server) Add(ctx context.Context, req *proto.Request) (*proto.Response, error) {
+	a, b := req.GetA(), req.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, req *proto.Request) (*proto.Response, error) {
+	a, b := req.GetA(), req.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
